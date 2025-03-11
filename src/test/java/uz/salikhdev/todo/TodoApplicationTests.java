f2c53e9a02a97f90b3ca6b9113a28ce587220435
Muhammadsolih Abdugafforov <salikhdev@gmail.com>
2025-03-11 17:21:39 +0500

    CI is successful 3

--- src/test/java/uz/salikhdev/todo/TodoApplicationTests.java
+++ src/test/java/uz/salikhdev/todo/TodoApplicationTests.java
@@ -2,12 +2,34 @@ package uz.salikhdev.todo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
+@ActiveProfiles("test")
 class TodoApplicationTests {
 
+    @Container
+    public static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.1")
+            .withDatabaseName("postgres")
+            .withUsername("user")
+            .withPassword("password");
+
+    @DynamicPropertySource
+    public static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
